a60f6ba4400f901b6adcf2c617f93bbaca929550
Beck-berry <trebeka98@gmail.com>
2020-09-24 10:13:18 +0200

    add base save search components

--- app/component/SavedSearchesPanel.js
+++ app/component/SavedSearchesPanel.js
@@ -1,8 +1,13 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import SavedSearchesList from './SavedSearchesList';
 
 function SavedSearchesPanel() {
-  return <div className="frontpage-panel fullscreen">Your saved searches.</div>;
+  return (
+    <div className="frontpage-panel fullscreen">
+      <SavedSearchesList />
+    </div>
+  );
 }
 
 SavedSearchesPanel.contextTypes = {
