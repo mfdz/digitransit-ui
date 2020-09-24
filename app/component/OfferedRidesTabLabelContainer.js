564d9f6c5a9702254ceff6163e9efb82c9044532
Mérő Mihály <meromisi88@gmail.com>
2020-09-24 10:31:33 +0200

    fixed carpool icon

--- app/component/OfferedRidesTabLabelContainer.js
+++ app/component/OfferedRidesTabLabelContainer.js
@@ -8,7 +8,13 @@ export default function OfferedRidesTabLabelContainer({ classes, onClick }) {
   return (
     // eslint-disable-next-line jsx-a11y/no-noninteractive-element-to-interactive-role, jsx-a11y/click-events-have-key-events
     <li className={classes} onClick={onClick} role="button">
-      <Icon className="prefix-icon" img="icon-icon_save" />
+      <Icon
+        className="prefix-icon"
+        img="icon-icon_carpool-withoutBox"
+        color="#005b8c"
+        height={1.3}
+        width={1.3}
+      />
       <FormattedMessage
         id="your-offered-rides"
         defaultMessage="Offered rides"
