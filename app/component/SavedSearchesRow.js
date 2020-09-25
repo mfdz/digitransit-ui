3e03a5bf6f62f017920fe24fb3adcfc4dbb716d1
Beck-berry <trebeka98@gmail.com>
2020-09-25 09:54:06 +0200

    use epoch time to display date and time on saved searches panel

--- app/component/SavedSearchesRow.js
+++ app/component/SavedSearchesRow.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import moment from 'moment';
 
 const SavedSearchesRow = ({
   from,
@@ -13,9 +14,11 @@ const SavedSearchesRow = ({
     <tr className="saved-search-row">
       <td className="saved-search-data">{from}</td>
       <td className="saved-search-data">{to}</td>
-      <td className="saved-search-data">{date}</td>
-      <td className="saved-search-data">{time}</td>
-      <td className="saved-search-data text-center">{maxtime}</td>
+      <td className="saved-search-data">
+        {moment.unix(date).format('DD.MM.YYYY')}
+      </td>
+      <td className="saved-search-data">{moment.unix(time).format('hh:mm')}</td>
+      <td className="saved-search-data text-center">{maxtime} Min</td>
       <td className="saved-search-data text-center">{passengerNumber}</td>
     </tr>
   );
