5eb73e32d7eb49ee0e12148f2d5a0fc703f562bf
Beck-berry <trebeka98@gmail.com>
2020-10-02 10:45:57 +0200

    refactor

--- app/component/SavedRow.js
+++ app/component/SavedRow.js
@@ -1,16 +1,11 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import moment from 'moment';
 
 const SavedRow = ({ from, to, date, time, passengerNumber, editable }) => {
   return (
     <tr className="saved-search-row">
-      <td className="saved-search-data text-center">
-        {moment.unix(date).format('DD.MM.YYYY')}
-      </td>
-      <td className="saved-search-data text-center">
-        {moment.unix(time).format('hh:mm')}
-      </td>
+      <td className="saved-search-data text-center">{date}</td>
+      <td className="saved-search-data text-center">{time}</td>
       <td className="saved-search-data">{from}</td>
       <td className="saved-search-data">{to}</td>
       {passengerNumber !== 0 && (
@@ -25,8 +20,8 @@ const SavedRow = ({ from, to, date, time, passengerNumber, editable }) => {
 SavedRow.propTypes = {
   from: PropTypes.string.isRequired,
   to: PropTypes.string.isRequired,
-  date: PropTypes.number.isRequired,
-  time: PropTypes.number.isRequired,
+  date: PropTypes.string.isRequired,
+  time: PropTypes.string.isRequired,
   passengerNumber: PropTypes.number,
   editable: PropTypes.bool,
 };
