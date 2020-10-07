8087ef8e12dd35fce1a0072179f1b1ff5e770bb3
Mérő Mihály <meromisi88@gmail.com>
2020-10-07 11:35:18 +0200

    edited variables to be in sync with backend

--- app/component/SavedRow.js
+++ app/component/SavedRow.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import Icon from './Icon';
 
-const SavedRow = ({ from, to, date, time, passengerNumber, toDetail, id }) => {
+const SavedRow = ({ from, to, date, time, seats, toDetail, id }) => {
   return (
     <tr
       className="saved-search-row text-center cursor-pointer"
@@ -12,9 +12,7 @@ const SavedRow = ({ from, to, date, time, passengerNumber, toDetail, id }) => {
       <td className="saved-search-data">{time}</td>
       <td className="saved-search-data">{from}</td>
       <td className="saved-search-data">{to}</td>
-      {passengerNumber !== 0 && (
-        <td className="saved-search-data">{passengerNumber}</td>
-      )}
+      {seats !== 0 && <td className="saved-search-data">{seats}</td>}
       <td>
         <Icon img="icon-icon_delete" />
       </td>
@@ -27,13 +25,13 @@ SavedRow.propTypes = {
   to: PropTypes.string.isRequired,
   date: PropTypes.string.isRequired,
   time: PropTypes.string.isRequired,
-  passengerNumber: PropTypes.number,
+  seats: PropTypes.number,
   toDetail: PropTypes.func,
   id: PropTypes.number,
 };
 
 SavedRow.defaultProps = {
-  passengerNumber: 0,
+  seats: 0,
 };
 
 SavedRow.contextTypes = {
