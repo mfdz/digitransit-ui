fd46bb39da1c20c0336aa1db166015977e00bde7
Beck-berry <trebeka98@gmail.com>
2020-10-07 12:59:58 +0200

    add login and pending screen to side panels

--- app/component/OfferedRidesPanel.js
+++ app/component/OfferedRidesPanel.js
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import OfferedRidesList from './OfferedRidesList';
 import OfferedRidesDetail from './OfferedRidesDetail';
+import LoginButton from './LoginButton';
+import Loading from './Loading';
 
 let currentRide;
 
 function OfferedRidesPanel() {
   const [renderComponent, setRenderComponent] = useState('list');
   const [data, setData] = useState([]);
+  const [formState, setFormState] = useState('pending');
+  const userLoggedIn = true;
 
   useEffect(() => {
     // TODO: change server URL
@@ -17,7 +21,12 @@ function OfferedRidesPanel() {
         headers: { 'content-type': 'application/json' },
       },
     )
-      .then(response => response.json())
+      .then(response => {
+        if (response.ok) {
+          setFormState('success');
+        }
+        return response.json();
+      })
       .then(d => (d ? setData(d) : setData([])))
       // eslint-disable-next-line no-console
       .catch(e => console.log(e));
@@ -31,6 +40,35 @@ function OfferedRidesPanel() {
   const toList = () => {
     setRenderComponent('list');
   };
+
+  const renderLogin = () => {
+    return (
+      <div className="sidePanelText">
+        Please log in to see your saved searches.
+        <LoginButton className="sidePanel-btn login-icon" isMobile />
+      </div>
+    );
+  };
+
+  if (!userLoggedIn) {
+    return renderLogin();
+  }
+
+  if (formState === 'pending') {
+    return (
+      <div className="frontpage-panel fullscreen">
+        <Loading />;
+      </div>
+    );
+  }
+
+  if (data.length < 0) {
+    return (
+      <div className="frontpage-panel fullscreen">
+        You donot have any saved searches yet.
+      </div>
+    );
+  }
 
   return (
     <div className="frontpage-panel fullscreen">
--- app/component/SavedSearchesPanel.js
+++ app/component/SavedSearchesPanel.js
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import SavedSearchesList from './SavedSearchesList';
 import SavedSearchDetail from './SavedSearchDetail';
+import LoginButton from './LoginButton';
+import Loading from './Loading';
 
 let currentSearch;
 
 const SavedSearchesPanel = () => {
   const [renderComponent, setRenderComponent] = useState('list');
   const [data, setData] = useState([]);
+  const [formState, setFormState] = useState('pending');
+  const userLoggedIn = true;
 
   useEffect(() => {
     // TODO: change server URL
@@ -17,7 +21,12 @@ const SavedSearchesPanel = () => {
         headers: { 'content-type': 'application/json' },
       },
     )
-      .then(response => response.json())
+      .then(response => {
+        if (response.ok) {
+          setFormState('success');
+        }
+        return response.json();
+      })
       .then(d => (d ? setData(d) : setData([])))
       // eslint-disable-next-line no-console
       .catch(e => console.log(e));
@@ -31,6 +40,35 @@ const SavedSearchesPanel = () => {
   const toList = () => {
     setRenderComponent('list');
   };
+
+  const renderLogin = () => {
+    return (
+      <div className="sidePanelText">
+        Please log in to see your saved searches.
+        <LoginButton className="sidePanel-btn login-icon" isMobile />
+      </div>
+    );
+  };
+
+  if (!userLoggedIn) {
+    return renderLogin();
+  }
+
+  if (formState === 'pending') {
+    return (
+      <div className="frontpage-panel fullscreen">
+        <Loading />;
+      </div>
+    );
+  }
+
+  if (data.length < 0) {
+    return (
+      <div className="frontpage-panel fullscreen">
+        You donot have any saved searches yet.
+      </div>
+    );
+  }
 
   return (
     <div className="frontpage-panel fullscreen">
