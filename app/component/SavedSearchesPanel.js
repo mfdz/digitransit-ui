9c9d5cb3c7c8e8b1737284310efab312f316da3c
Mérő Mihály <meromisi88@gmail.com>
2020-10-07 10:23:15 +0200

    fetching data from server

--- app/component/SavedSearchesPanel.js
+++ app/component/SavedSearchesPanel.js
@@ -1,13 +1,25 @@
 import PropTypes from 'prop-types';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import SavedSearchesList from './SavedSearchesList';
 import SavedSearchDetail from './SavedSearchDetail';
-import list from '../../static/assets/MOCK_DATA';
 
 let currentId;
 
 const SavedSearchesPanel = () => {
   const [renderComponent, setRenderComponent] = useState('list');
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    // TODO: change server URL
+    fetch('http://localhost:3000/itineraries', {
+      method: 'GET',
+      headers: { 'content-type': 'application/json' },
+    })
+      .then(response => response.json())
+      .then(d => (d ? setData(d) : setData([])))
+      // eslint-disable-next-line no-console
+      .catch(e => console.log(e));
+  }, []);
 
   const toDetail = id => {
     currentId = id;
@@ -21,7 +33,7 @@ const SavedSearchesPanel = () => {
   return (
     <div className="frontpage-panel fullscreen">
       {renderComponent === 'list' ? (
-        <SavedSearchesList list={list} toDetail={toDetail} />
+        <SavedSearchesList list={data} toDetail={toDetail} />
       ) : (
         <SavedSearchDetail toList={toList} id={currentId} />
       )}
